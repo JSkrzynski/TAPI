8de806addd9572df6016080769dec2c176627e43
JSkrzynski <jakubskrzynski18@gmail.com>
2023-10-26 16:07:21 +0200

    api

--- server/student.js
+++ server/student.js
@@ -2,24 +2,31 @@ import express from "express";
 import {faker} from "@faker-js/faker";
 
 export const studentRouter = express.Router();
+const schedule = [];
 
-studentRouter.get('/:id', (req,res) => {
-    const id = req.params.id;
-    res.send("Student "+id);
-});
 studentRouter.get('/:id/plan', (req,res) => {
+    for (let i = 0; i < 5; i++){
+        schedule[i] = {
+            date: faker.date.anytime(),
+            subject: faker.commerce.productName(),
+            classroom: faker.number.int({min:101,max:600})
+        }
+    }
     res.json({
         name: faker.person.fullName(),
-        date: faker.date.anytime,
-        subject: faker.commerce.productName(),
-        classroom: faker.number.int({min:101,max:600})
+        schedule: schedule
     });
 })
 studentRouter.get('/:id/plan/:from/:to', (req,res) => {
+    for (let i = 0; i < 2; i++){
+        schedule[i] = {
+            date: faker.date.between({ from: req.params.from+'T08:00:00.000Z', to: req.params.to+'T20:00:00.000Z' }),
+            subject: faker.commerce.productName(),
+            classroom: faker.number.int({min:101,max:600})
+        }
+    }
     res.json({
         name: faker.person.fullName(),
-        date: faker.date.between({ from: req.params.from+'T08:00:00.000Z', to: req.params.to+'T20:00:00.000Z' }),
-        subject: faker.commerce.productName(),
-        classroom: faker.number.int({min:101,max:600})
+        schedule: schedule
     });
 })
